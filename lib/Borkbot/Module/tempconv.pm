135850a425f2b3011dc923bf2021be10a62c78a4
Andrew Rodland <andrew@cleverdomain.org>
2017-03-11 01:19:33 -0500

    Fix C to K offset

--- lib/Borkbot/Module/tempconv.pm
+++ lib/Borkbot/Module/tempconv.pm
@@ -12,7 +12,7 @@ sub on_irc_privmsg {
     return 1;
   }
   elsif($ev->msg =~ /^\.ftok\s+(\-?[\d]+(\.[\d]+)?)/i) {
-    my $temp = (5/9)*($1-32) + 273;
+    my $temp = (5/9)*($1-32) + 273.15;
     $temp = sprintf("%.2f",$temp);
     $self->irc->privmsg($ev->reply_to, "$1 F = $temp K");
     return 1;
@@ -24,19 +24,19 @@ sub on_irc_privmsg {
     return 1;
   }
   elsif($ev->msg =~ /^\.ctok\s+(\-?[\d]+(\.[\d]+)?)/i) {
-    my $temp = $1 + 273;
+    my $temp = $1 + 273.15;
     $temp = sprintf("%.2f",$temp);
     $self->irc->privmsg($ev->reply_to, "$1 C = $temp K");
     return 1;
   }
   elsif($ev->msg =~ /^\.ktof\s+(\-?[\d]+(\.[\d]+)?)/i) {
-    my $temp = ((9/5)*($1-273)) + 32;
+    my $temp = ((9/5)*($1-273.15)) + 32;
     $temp = sprintf("%.2f",$temp);
     $self->irc->privmsg($ev->reply_to, "$1 K = $temp F");
     return 1;
   }
   elsif($ev->msg =~ /^\.ktoc\s+(\-?[\d]+(\.[\d]+)?)/i) {
-    my $temp = $1 - 273;
+    my $temp = $1 - 273.15;
     $temp = sprintf("%.2f",$temp);
     $self->irc->privmsg($ev->reply_to, "$1 K = $temp C");
     return 1;
