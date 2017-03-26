f23d2ca4247cf5365375570aaf82b76a0f571dfa
David.Mackintosh <dave@xdroop.com>
2017-03-26 03:27:07 -0400

    catfact module

---
+++ lib/Borkbot/Module/catfact.pm
@@ -0,0 +1,29 @@
+package Borkbot::Module::catfact;
+use Moo;
+use Borkbot::Module;
+use Mojo::UserAgent;
+
+sub on_irc_privmsg {
+  my ($self, $ev) = @_;
+
+  return 0 unless $ev->{msg} =~ /^\.catfact\s*$/i;
+
+  $self->ua->get('https://catfacts-api.appspot.com/api/facts' => sub {
+      my ($ua, $tx) = @_;
+      my $res = $tx->result;
+      if ($res->is_error) {
+        $self->irc->privmsg($ev->{reply_to}, "Catfact can has error: " . $res->message);
+        return;
+      }
+      my $fact = $res->headers->body;
+      # it's actually JSON, but ain't nobody got time fo that shit
+      $fact =~ s/^.*\[//;
+      $fact =~ s/\].*$//;
+      $self->irc->privmsg($ev->{reply_to}, "Thank you for subscribing to CatFacts(tm)!");
+      $self->irc->privmsg($ev->{reply_to}, $fact);
+  });
+
+  return 1;
+}
+
+1;
